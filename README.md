eacf4c520bff54263c2e54be5d11246168840fa3
rabius-sunny <rabiussunnyc3x@gmail.com>
2022-06-15 14:09:19 +0600

    firebase removal

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-# Portfolio with React and Material UI
+# Portfolio with Reactjs and Material UI
 <p> This is the official portfolio of Rabius Sunny</p>
-<p> This project is open source for educational purpose only</p>
-<p>Check out the <a href="https://rabius-sunny.web.app">live link here.</a> </p>
+<p>Check out the <a href="https://rabius-sunny.netlify.app">live site here.</a> </p>
--- src/components/Home/intro/Intro.jsx
+++ src/components/Home/intro/Intro.jsx
@@ -1,47 +1,62 @@
-import Typical from "react-typical";
+import Typical from 'react-typical'
 import Grid from '@material-ui/core/Grid'
 import { Container } from '@material-ui/core'
 import './intro.css'
-import img from '../../../images/pic2.png'
+import img from '../../../images/propicc.jpg'
 import { Link } from 'react-router-dom'
 
 function Intro() {
-    return (
-        <section className="intro">
-            <Container>
-                <Grid container className="display">
-                    <Grid item md={5}>
-                        <div className="dudeImg">
-                            <img src={img} alt="dude" />
-                        </div>
-                    </Grid>
-                    <Grid item md={7}>
-                        <div className="dudeBio">
-                            <p>
-                                <Typical
-                                    steps={[
-                                        "Front end designer", 2000,
-                                        "Back end developer", 2000,
-                                        "Full-stack developer", 2000,
-                                        "Writer", 2000
-                                    ]}
-                                    loop={Infinity}
-                                />
-                            </p>
-                            <h1>Rabius Sunny</h1>
-                            <p>Junior Web Developer adept at contributing to collaborative working environments, finding
-                            solutions and determining customer satisfaction. Designed and developed web applications across the latest technologies and tools, multiple APIs, third-party
-                            integrations and databases meeting the highest standards, best
-                            practices and speed.</p>
-                            <div className="dudeBtn">
-                                <a href="https://drive.google.com/uc?export=download&id=1SoEau1dfnTpKhf9axwTZ5-ofTKWF6B-B" target="_blank" rel="noreferrer">Download CV</a><Link to="/contact">Contact</Link>
-                            </div>
-                        </div>
-                    </Grid>
-                </Grid>
-            </Container>
-        </section>
-    )
+  return (
+    <section className='intro'>
+      <Container>
+        <Grid container className='display'>
+          <Grid item md={5}>
+            <div className='dudeImg'>
+              <img src={img} alt='dude' />
+            </div>
+          </Grid>
+          <Grid item md={7}>
+            <div className='dudeBio'>
+              <p>
+                <Typical
+                  steps={[
+                    'Front end developer',
+                    2000,
+                    'Back end developer',
+                    2000,
+                    'Full-stack developer',
+                    2000,
+                    'Writer',
+                    2000
+                  ]}
+                  loop={Infinity}
+                />
+              </p>
+              <h1>Rabius Sunny (rezoan)</h1>
+              <p>
+                Web Developer adept at contributing to collaborative working
+                environments, finding solutions and determining customer
+                satisfaction. Designed and developed web applications across the
+                latest technologies and tools, multiple APIs, third-party
+                integrations and databases meeting the highest standards, best
+                practices and speed.
+              </p>
+              <div className='dudeBtn'>
+                <a
+                  href='https://drive.google.com/uc?export=download&id=1ZJdc3z2_y29-UaU6qU7I8HnMQxr31sVl'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  Download CV
+                </a>
+                <Link to='/contact'>Contact</Link>
+              </div>
+            </div>
+          </Grid>
+        </Grid>
+      </Container>
+    </section>
+  )
 }
 
 export default Intro
